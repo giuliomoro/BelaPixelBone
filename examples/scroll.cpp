06f9649f7b34fde6d936d80b038125aeb9c86b7f
clwe <clemens@chair.audio>
2023-11-24 19:07:27 +0100

    slow down scroll example

--- examples/scroll.cpp
+++ examples/scroll.cpp
@@ -9,13 +9,14 @@
 #include "../pixel.hpp"
 
 int main(void) {
-  const uint32_t num_pixels = 384;
+  const uint32_t num_pixels = 8;
   PixelBone_Pixel *const strip = new PixelBone_Pixel(num_pixels);
   while (1) {
     for (uint32_t p = 0; p < num_pixels; p++) {
       strip->clear();
       strip->setPixelColor(p, PixelBone_Pixel::Color(128, 128, 128));
       strip->show();
+      usleep(50000);
     }
   }
 
