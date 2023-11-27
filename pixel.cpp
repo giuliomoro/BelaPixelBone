a13c31ecce4592e0f46fa3ea86ce1aa1ea439d58
Giulio Moro <giuliomoro@yahoo.it>
2023-11-27 10:15:16 +0000

    Use pasm-generated _bin.h instead of the .bin, so it is just works with Bela's build system

--- pixel.cpp
+++ pixel.cpp
@@ -1,6 +1,7 @@
 #include "pixel.hpp"
 #include <iostream>
 #include <cstring>
+#include "ws281x_bin.h"
 
 /* GPIO pins used */
 static const uint8_t gpios0 = 8; // this is P8.35 on beagle bone
@@ -19,7 +20,7 @@ PixelBone_Pixel::PixelBone_Pixel(uint16_t pixel_count)
   pru_gpio(0, gpios0, 1, 0);
 
   // Initiate the PRU0 program
-  pru_exec(pru0, "./ws281x.bin");
+  pru_exec_code(pru0, PRUcode, sizeof(PRUcode));
 
   // Watch for a done response that indicates a proper startup
   // TODO: timeout if it fails
